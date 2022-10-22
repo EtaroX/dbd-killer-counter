add1653aab5f5fa1ebc5f95bf5e2c2364e32cee0
EtaroX <maciejkowo5687@gmail.com>
2022-10-22 17:46:19 +0200

    autoload for phpmailer

---
+++ dlc/vendor/PHPmail/autoload.php
@@ -0,0 +1,7 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require_once ("phpMailer/PHPMailer.php");
+require_once ("phpMailer/SMTP.php");
+require_once ("phpMailer/Exception.php");
