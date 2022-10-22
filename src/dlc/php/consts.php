d1c9acf5f43a57ef2298595f1641936a626cd206
EtaroX <maciejkowo5687@gmail.com>
2022-10-22 20:33:43 +0200

    joe

--- src/dlc/php/consts.php
+++ src/dlc/php/consts.php
@@ -1,6 +1,6 @@
 <?php
 define("HOST", $_SERVER['HTTP_HOST']);
-define("PODFOLDER", "/technoblade/dbd-killer-counter/");
+define("PODFOLDER", "/technoblade/dbd-killer-counter/src");
 define("SECUREONLY",false);
 
 
